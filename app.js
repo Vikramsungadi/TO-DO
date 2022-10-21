2f96a83c79ffab2fc9c14a4779c4c67a1dd0a5bc
Vikram Sungadi <55626981+Vikramsungadi@users.noreply.github.com>
2022-10-21 14:13:29 +0530

    Improved Styling Tweaked Editing Functionality

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const trashicon = document.querySelector("i");
 const bottomCard = document.querySelector(".bottom--card");
 const pa = document.querySelectorAll(".para");
 
+//                          Event Listeners
+
 button.addEventListener("click", addTask);
 main.addEventListener("click", removeTask);
 main.addEventListener("click", checkTask);
 main.addEventListener("click", edit);
-// main.addEventListener("click", previous);
-main.addEventListener("click", done);
+
 document.addEventListener("DOMContentLoaded", loadTasksFromLS);
 
-// CREATING TASK COMPONENT
+//                          CREATING TASK COMPONENT
 
 function creatingTaskComponent(text) {
   const card = document.createElement("div");
@@ -21,7 +22,6 @@ function creatingTaskComponent(text) {
   const p = document.createElement("p");
   p.className = "para";
   p.innerText = text;
-  // p.appendChild(document.createTextNode(text));
   const trash = document.createElement("i");
   trash.className = "fa-solid fa-trash";
   const check = document.createElement("i");
@@ -31,18 +31,13 @@ function creatingTaskComponent(text) {
   const icon = document.createElement("div");
   icon.className = "icon";
   icon.append(check, trash, edit);
-  // console.log(icon);
-  // trash.className = "fa-solid fa-trash";
-  // const check = document.createElement("div");
-  // check.className = "check";
-  // check.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
 
   card.append(p, icon);
 
   main.appendChild(card);
 }
 
-// LOADING TASKS FROM LOCAL STORAGE TO DOM
+//              LOADING TASKS FROM LOCAL STORAGE TO DOM
 
 function loadTasksFromLS() {
   if (localStorage.getItem("tasks") !== null) {
@@ -60,8 +55,6 @@ function loadTasksFromLS() {
       }
       p.innerText = task.Text;
 
-      // console.log(task.Text);
-      // p.appendChild(document.createTextNode(text));
       const trash = document.createElement("i");
       trash.className = "fa-solid fa-trash";
       const check = document.createElement("i");
@@ -70,13 +63,6 @@ function loadTasksFromLS() {
       edit.className = "fa-solid fa-pen-to-square";
       const icon = document.createElement("div");
       icon.className = "icon";
-
-      // if (task.strike === true) {
-      //   icon.append(check, trash);
-      // } else {
-      //   icon.append(check, trash, edit);
-      // }
-      // console.log(icon);
       icon.append(check, trash, edit);
       card.append(p, icon);
 
@@ -85,7 +71,7 @@ function loadTasksFromLS() {
   }
 }
 
-// ADDING TASK TO DOM
+//                          ADDING TASK TO DOM
 
 function addTask() {
   if (taskinput.value === "") {
@@ -97,7 +83,7 @@ function addTask() {
   }
 }
 
-// STORING TASK INPUT IN LOCAL STORAGE
+//                     STORING USER GIVEN TASK IN LOCAL STORAGE
 
 function storeTaskinLS(task) {
   let tasks;
@@ -108,26 +94,23 @@ function storeTaskinLS(task) {
   }
   obj = { Text: task, strike: false };
   tasks.push(obj);
-  // tasks.push(task);
-  // console.log(obj.Text);
+
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-//  REMOVING TASK COMPONENT FROM DOM
+//                      REMOVING TASK COMPONENT FROM DOM
 
 function removeTask(e) {
-  // console.log(e.target.className);
   if (
     e.target.parentElement.parentElement.classList.contains("bottom--card") &&
     e.target.classList.contains("fa-trash")
   ) {
     e.target.parentElement.parentElement.remove();
-    console.log(e.target.parentElement.parentElement.textContent);
     removeTaskFromLS(e.target.parentElement.parentElement.textContent);
   }
 }
 
-//  REMOVING TASK INPUT IN LOCAL STORAGE
+//                   function for REMOVING TASK INPUT IN LOCAL STORAGE
 
 function removeTaskFromLS(text) {
   let tasks;
@@ -135,18 +118,18 @@ function removeTaskFromLS(text) {
   tasks.forEach(function (task, index) {
     if (text === task.Text) {
       tasks.splice(index, 1);
-      // console.log(text == task.Text);
     }
     localStorage.setItem("tasks", JSON.stringify(tasks));
   });
 }
+
+//                                      MarkingTask
 
 function checkTask(e) {
   if (
     e.target.parentElement.parentElement.classList.contains("bottom--card") &&
     e.target.classList.contains("fa-circle-check")
   ) {
-    // console.log(e.target.parentElement.parentElement.querySelector(".para"));
     const card = e.target.parentElement.parentElement;
     const p = e.target.parentElement.parentElement.querySelector("p");
     let tasks = JSON.parse(localStorage.getItem("tasks"));
@@ -154,16 +137,16 @@ function checkTask(e) {
     if (p.classList.contains("strike")) {
       p.classList.remove("strike");
       card.classList.remove("strike--card");
-      // e.target.parentElement.lastChild.style.display = "block";
       storeStrikesToLS(p.textContent);
     } else {
       p.classList.add("strike");
       card.classList.add("strike--card");
-      // e.target.parentElement.lastChild.style.display = "none";
       storeStrikesToLS(p.textContent);
     }
   }
 }
+
+//                    Storing Marked Tasks to LS
 
 function storeStrikesToLS(text) {
   let tasks = JSON.parse(localStorage.getItem("tasks"));
@@ -179,6 +162,8 @@ function storeStrikesToLS(text) {
   });
 }
 
+//                    Editing Task and Storing to LS
+
 function edit(e) {
   let grandparent = e.target.parentElement.parentElement;
   let parent = e.target.parentElement;
@@ -186,47 +171,55 @@ function edit(e) {
     grandparent.classList.contains("bottom--card", "top--card") &&
     e.target.classList.contains("fa-pen-to-square")
   ) {
-    console.log(parent.previousElementSibling.textContent);
     let p = parent.previousElementSibling;
-    console.log(p);
     let textp = p.textContent;
-    p.classList.add("none");
-    parent.classList.add("none");
+
     grandparent.innerHTML = `<div class="input--holder">
           <div class="btn--div">
             <input class="input input2"  value="${textp}" type="text" />
             <button type="submit" class="btn2">Done</button>
           </div>
         </div>`;
-    removeTaskFromLS(textp);
+
+    return main.addEventListener("click", function (e) {
+      if (e.target.classList.contains("btn2")) {
+        let text = e.target.previousElementSibling.value;
+        let x = e.target.closest(".bottom--card");
+
+        UpdateTask(x, textp, text);
+      }
+    });
   }
 }
-// function previous(e) {
-//   if (
-//     e.target.parentElement.classList.contains("bottom--card", "top--card") &&
-//     e.target.classList.contains("para")
-//   )
-//     return target.textContent;
-// }
 
-function done(e) {
-  if (e.target.classList.contains("btn2")) {
-    let text = e.target.previousElementSibling.value;
-    console.log(text);
-    let x = e.target.closest(".bottom--card");
-    x.innerHTML = `<p class="para">${text}</p>
-          <div class="icon checked--icons">
-            <i class="fa-regular fa-circle-check"></i
-            ><i class="fa-solid fa-trash"></i><i class="fa-solid fa-pen-to-square">
-          </div>`;
-    console.log(x);
-    storeTaskinLS(text);
-    //   let tasks = JSON.parse(localStorage.getItem("tasks"));
-    //   tasks.forEach(function (task) {
-    //     if (task.Text === previousText) {
-    //       task.Text = text;
-    //     }
-    //   });
-    //   localStorage.setItem("tasks", JSON.stringify(tasks));
-  }
+//                           function for Updating task in LS and DOM
+
+function UpdateTask(x, previousvalue, text) {
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  tasks.forEach(function (task) {
+    if (task.Text === previousvalue) {
+      task.Text = text;
+      let striked = task.strike;
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+
+      // creating div after editing
+      let p = document.createElement("p");
+      p.classList.add("para");
+      //        if edit opiton is enabled even after striking use below
+
+      // if (striked) {
+      //   p.classList.add("para", "strike");
+      // } else {
+      //   p.classList.add("para");
+      // }
+      p.innerText = text;
+      div = document.createElement("div");
+      div.classList.add("icon", "checked--icons");
+      div.innerHTML = `<i class="fa-regular fa-circle-check"></i
+                    ><i class="fa-solid fa-trash"></i><i class="fa-solid fa-pen-to-square">`;
+
+      x.innerHTML = "";
+      x.append(p, div);
+    }
+  });
 }
